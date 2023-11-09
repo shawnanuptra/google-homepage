9ef61d11e15fb0e3aa05c4f1c1a94d99207f02fa
Shawn Martin <bh74sa@student.sunderland.ac.uk>
2023-11-09 13:45:36 +0000

    added javascript to toggle Search Bar style

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+
+
+const textArea = document.getElementsByClassName('searchText')[0];
+textArea.onfoc('onclic')
+
+// used to change bgcolor of Search Bar when TextArea is on focus.
+function changeBackgroundColour(focus) {
+    // get element
+    const search = document.getElementsByClassName('search')[0]
+
+    focus ? search.classList.add('textAreaActive') : search.classList.remove('textAreaActive');
+}
